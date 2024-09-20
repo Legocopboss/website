66cdfbe11514e4a74ebcf7a6595d40131a49c680
Braden Pool <legocopboss@gmail.com>
2024-09-20 14:05:03 -0400

    made some adjustments

--- scripts.js
+++ scripts.js
@@ -12,3 +12,11 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+function scrollToSection(sectionId) {
+    const targetSection = document.getElementById(sectionId);
+    window.scrollTo({
+        top: targetSection.offsetTop - 60,
+        behavior: 'smooth'
+    });
+}
