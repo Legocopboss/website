41a2f95031d7c32aaee6405757260b449466797e
Braden Pool <legocopboss@gmail.com>
2024-09-20 14:25:16 -0400

    made soem changes
    formattign is horrible why am i making a website

--- scripts.js
+++ scripts.js
@@ -1,18 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const links = document.querySelectorAll('nav ul li a');
-    links.forEach(link => {
-        link.addEventListener('click', function(e) {
-            e.preventDefault();
-            const targetId = this.getAttribute('href').substring(1);
-            const targetSection = document.getElementById(targetId);
-            window.scrollTo({
-                top: targetSection.offsetTop - 60,
-                behavior: 'smooth'
-            });
-        });
-    });
-});
-
 function scrollToSection(sectionId) {
     const targetSection = document.getElementById(sectionId);
     window.scrollTo({
