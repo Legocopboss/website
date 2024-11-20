e651ae67ff6668573bdd178fbece5d7ce918dd4b
Braden Pool <legocopboss@gmail.com>
2024-11-20 13:27:31 -0500

    tried making some changes so it works with aws

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from './App';
 import "./index.css";
+import { Amplify } from "aws-amplify";
+import outputs from "../amplify_outputs.json";
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+Amplify.configure(outputs);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
